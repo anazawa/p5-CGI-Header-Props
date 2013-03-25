ea488c94e53f01aecebf8fd0849ab8c2ddeccb6f
Ryo Anazawa <anazawa@cpan.org>
2013-03-26 05:14:13 +0900

    use numeric compare

--- lib/CGI/Application/Plugin/Header.pm
+++ lib/CGI/Application/Plugin/Header.pm
@@ -19,7 +19,7 @@ sub header {
     if ( !$self->{__HEADER_PROPS} ) {
         $self->{__HEADER_PROPS} = $header->header; # initialize
     }
-    elsif ( $self->{__HEADER_PROPS} ne $header->header ) {
+    elsif ( $self->{__HEADER_PROPS} != $header->header ) {
         $header->clear->set( $self->header_props );
         $self->{__HEADER_PROPS} = $header->header; # overwrite
     }
@@ -45,7 +45,7 @@ __END__
 
 =head1 NAME
 
-CGI::Application::Plugin::Header -
+CGI::Application::Plugin::Header - Plugin for handling header props.
 
 =head1 SYNOPSIS
 
@@ -74,11 +74,56 @@ CGI::Application::Plugin::Header -
       $header->set( $key => $value );
       $header->delete( $key );
 
-      # compatible with CGI::Application's core methods
+      # compatible with the core methods of CGI::Applications
       $self->header_props( $key => $value, ... );
       $self->header_add( $key => $value );
+      $self->header_type( 'redirect' );
   }
 
   ...
+
+=head1 DESCRIPTION
+
+This plugin provides a way to handle CGI.pm-compatible HTTP header
+properties.
+
+=head2 METHODS
+
+=over 4
+
+=item $cgiapp->header
+
+Returns a L<CGI::Header::Props> object.
+
+=item $value = $cgiapp->header( $prop )
+
+A shortcut for:
+
+  $value = $cgiapp->header->get( $prop );
+
+=item $cgiapp->header( $p1 => $v1, $p2 => $v2, ... )
+
+A shortcut for:
+
+  $cgiapp->header->set(
+      $p1 => $v1,
+      $p2 => $v2,
+      ...
+  );
+
+=back
+
+=head1 SEE ALSO
+
+L<CGI::Application>, L<CGI::Header::Props>
+
+=head1 AUTHOR
+
+Ryo Anazawa (anazawa@cpan.org)
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
 
 =cut
